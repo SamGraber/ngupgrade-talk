3a2cb234caea941b681e930a5ca189b21a223e58
SamGraber <authorsamgraber@gmail.com>
2017-02-08 14:00:02 -0500

    Calculate the pace and show the pace and time as minutes and seconds
    We can do this by creating js date objects with them and using angular to filter them for display.

--- source/timeList/timeList.js
+++ source/timeList/timeList.js
@@ -10,7 +10,28 @@
 	timeListController.$inject = ['timeService'];
 	function timeListController(timeService) {
 		var self = this;
-		timeService.getTimeList().then(data => self.timeList = data);
+		timeService.getTimeList().then(data => self.timeList = data.map(calculatePace).map(formatTimes));
+
+		function calculatePace(time) {
+			var pace = time.time / time.distance;
+			return {
+				pace,
+				time: time.time,
+				distance: time.distance,
+			};
+		}
+
+		function formatTimes(time) {
+			return {
+				pace: toDate(time.pace),
+				time: toDate(time.time),
+				distance: time.distance,
+			};
+		}
+
+		function toDate(minutes) {
+			return new Date(minutes * 60000);
+		}
 	}
 
 }());
