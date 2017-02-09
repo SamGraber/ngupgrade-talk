84a7041999872fcf76900648622057f35c2c81cb
SamGraber <authorsamgraber@gmail.com>
2017-02-09 16:17:27 -0500

    Load in angular files through the bundle
    With any files that only interface through angular, we can simply switch them over to loading through webpack instead of the index page. An important thing to note is that this isn't going to work for files with globals. Once the file is imported this way, it becomes an es6 module file, and declarations are no longer considered globals. For this reason, we can't simply move utility.js over to this strategy.

---
+++ source/index.ts
@@ -0,0 +1,7 @@
+import './app.module';
+import './app.config';
+import './timeList/timeList';
+import './addTime/addTime';
+import './goal/goal';
+import './services/time.service';
+import './services/goal.service';
