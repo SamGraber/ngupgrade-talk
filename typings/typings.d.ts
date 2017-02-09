fc09160356bf9ef77c623ff53ad6a3358d364857
SamGraber <authorsamgraber@gmail.com>
2017-02-09 15:52:32 -0500

    Added needed typings
    Since we're using chai on top of jasmine, we have to define our own jasmine typings instead of installing the jasmine typings package. Declare a few globals for chai and angular-mocks.

---
+++ typings/typings.d.ts
@@ -0,0 +1,11 @@
+// Jasmine
+declare var describe;
+declare var it;
+declare var beforeEach;
+declare var afterEach;
+
+// Chai globals
+declare var expect: Chai.ExpectStatic;
+
+// angular-mocks globals
+declare var module;
