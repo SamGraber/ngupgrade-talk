50e617ce3ff8b3407718fe91d324b026abbdb63d
SamGraber <authorsamgraber@gmail.com>
2017-02-09 16:13:04 -0500

    Serve the app using webpack dev server instead of lite-server
    This gets us setup to convert over to webpack piece-by-piece. We'll add an index.ts and vendor.ts files just as placeholders so we can eventually use them to pull in our files into a two main bundles instead of loading them all separately.

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	debug: true,
+	devtool: 'source-map',
+	noInfo: false,
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+	},
+	entry: {
+		vendor: path.resolve(__dirname, '../source/vendor'),
+		main: path.resolve(__dirname, '../source/index'),
+	},
+	target: 'web',
+	output: {
+		path: path.resolve(__dirname, '../source'),
+		publicPath: '/',
+		filename: '[name].js',
+	},
+	plugins: [
+		// Use CommonsChunkPlugin to create a separate bundle
+		// of vendor libraries so that they're cached separately.
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+		}),
+
+		// Create HTML file that includes reference to bundled JS.
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+			inject: true,
+		}),
+	],
+	module: {
+		loaders: [
+			{ test: /\.ts$/, exclude: /node_modules/, loaders: ['awesome-typescript-loader'] },
+		],
+		preLoaders: [
+			{ test: /\.js$/, loader: 'source-map-loader' },
+		],
+	},
+};
---
+++ config/webpack.config.prod.js
@@ -0,0 +1,67 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var WebpackMd5Hash = require('webpack-md5-hash');
+
+module.exports = {
+	debug: true,
+	devtool: 'source-map',
+	noInfo: false,
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+	},
+	entry: {
+		vendor: path.resolve(__dirname, '../client/vendor'),
+		main: path.resolve(__dirname, '../client/app.module'),
+	},
+	target: 'web',
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/',
+		filename: '[name].[chunkhash].js',
+	},
+	plugins: [
+		// Hash the files using MD5 so that their names change when the content changes.
+		new WebpackMd5Hash(),
+
+		// Use CommonsChunkPlugin to create a separate bundle
+		// of vendor libraries so that they're cached separately.
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+		}),
+
+		// Create HTML file that includes reference to bundled JS.
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeSylteLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true,
+			},
+			inject: true,
+			production: true,
+		}),
+
+		// Eliminate duplicate packages when generating bundle
+		new webpack.optimize.DedupePlugin(),
+
+		// Minify JS
+		new webpack.optimize.UglifyJsPlugin(),
+	],
+	module: {
+		loaders: [
+			{ test: /\.ts$/, exclude: /node_modules/, loaders: ['awesome-typescript-loader'] },
+		],
+		preLoaders: [
+			{ test: /\.js$/, loader: 'source-map-loader' },
+		],
+	},
+};
