cb6c2eb65b9aba2714c737eef9f3613dd6545e49
SamGraber <authorsamgraber@gmail.com>
2017-02-10 13:50:19 -0500

    Fix test setup

--- config/karma-test-setup.js
+++ config/karma-test-setup.js
@@ -3,7 +3,19 @@ Error.stackTraceLimit = Infinity;
 require('angular/angular.js');
 require('angular-mocks/angular-mocks.js');
 
-require('../source/index');
+require('core-js/es6');
+require('core-js/es7/reflect');
+
+require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy');
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch');
+require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
+
+require('../source/timeList/timeList');
+require('../source/goal/goal');
 
 window.expect = chai.expect;
 
--- config/webpack.config.test.js
+++ config/webpack.config.test.js
@@ -6,9 +6,17 @@ module.exports = {
 	resolve: {
 		extensions: ['.webpack.js', '.web.js', '.ts', '.js']
 	},
+	plugins: [
+		// Workaround for angular/angular#11580
+		new webpack.ContextReplacementPlugin(
+			/angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+			__dirname
+		),
+	],
 	module: {
 		loaders: [
-			{ test: /\.ts$/, exclude: /node_modules/, loaders: ['awesome-typescript-loader'] },
+			{ test: /\.ts$/, exclude: /node_modules/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
+			{ test: /\.html$/, loader: 'raw-loader' },
 		],
 		preLoaders: [
 			{ test: /\.js$/, loader: 'source-map-loader' },
